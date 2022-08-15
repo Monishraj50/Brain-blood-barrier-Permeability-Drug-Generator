1ed01af0516eba44f8976aa84de5648854f342c8
Monish <53029918+Monishraj50@users.noreply.github.com>
2022-08-15 12:49:25 +0530

    Create wgan.py

---
+++ model/wgan.py
@@ -0,0 +1,106 @@
+
+
+class GraphWGAN(keras.Model):
+    def __init__(
+        self,
+        generator,
+        discriminator,
+        discriminator_steps=1,
+        generator_steps=1,
+        gp_weight=10,
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.generator = generator
+        self.discriminator = discriminator
+        self.discriminator_steps = discriminator_steps
+        self.generator_steps = generator_steps
+        self.gp_weight = gp_weight
+        self.latent_dim = self.generator.input_shape[-1]
+
+    def compile(self, optimizer_generator, optimizer_discriminator, **kwargs):
+        super().compile(**kwargs)
+        self.optimizer_generator = optimizer_generator
+        self.optimizer_discriminator = optimizer_discriminator
+        self.metric_generator = keras.metrics.Mean(name="loss_gen")
+        self.metric_discriminator = keras.metrics.Mean(name="loss_dis")
+
+    def train_step(self, inputs):
+
+        if isinstance(inputs[0], tuple):
+            inputs = inputs[0]
+
+        graph_real = inputs
+
+        self.batch_size = tf.shape(inputs[0])[0]
+
+        # Train the discriminator for one or more steps
+        for _ in range(self.discriminator_steps):
+            z = tf.random.normal((self.batch_size, self.latent_dim))
+
+            with tf.GradientTape() as tape:
+                graph_generated = self.generator(z, training=True)
+                loss = self._loss_discriminator(graph_real, graph_generated)
+
+            grads = tape.gradient(loss, self.discriminator.trainable_weights)
+            self.optimizer_discriminator.apply_gradients(
+                zip(grads, self.discriminator.trainable_weights)
+            )
+            self.metric_discriminator.update_state(loss)
+
+        # Train the generator for one or more steps
+        for _ in range(self.generator_steps):
+            z = tf.random.normal((self.batch_size, self.latent_dim))
+
+            with tf.GradientTape() as tape:
+                graph_generated = self.generator(z, training=True)
+                loss = self._loss_generator(graph_generated)
+
+                grads = tape.gradient(loss, self.generator.trainable_weights)
+                self.optimizer_generator.apply_gradients(
+                    zip(grads, self.generator.trainable_weights)
+                )
+                self.metric_generator.update_state(loss)
+
+        return {m.name: m.result() for m in self.metrics}
+
+    def _loss_discriminator(self, graph_real, graph_generated):
+        logits_real = self.discriminator(graph_real, training=True)
+        logits_generated = self.discriminator(graph_generated, training=True)
+        loss = tf.reduce_mean(logits_generated) - tf.reduce_mean(logits_real)
+        loss_gp = self._gradient_penalty(graph_real, graph_generated)
+        return loss + loss_gp * self.gp_weight
+
+    def _loss_generator(self, graph_generated):
+        logits_generated = self.discriminator(graph_generated, training=True)
+        return -tf.reduce_mean(logits_generated)
+
+    def _gradient_penalty(self, graph_real, graph_generated):
+        # Unpack graphs
+        adjacency_real, features_real = graph_real
+        adjacency_generated, features_generated = graph_generated
+
+        # Generate interpolated graphs (adjacency_interp and features_interp)
+        alpha = tf.random.uniform([self.batch_size])
+        alpha = tf.reshape(alpha, (self.batch_size, 1, 1, 1))
+        adjacency_interp = (adjacency_real * alpha) + (1 - alpha) * adjacency_generated
+        alpha = tf.reshape(alpha, (self.batch_size, 1, 1))
+        features_interp = (features_real * alpha) + (1 - alpha) * features_generated
+
+        # Compute the logits of interpolated graphs
+        with tf.GradientTape() as tape:
+            tape.watch(adjacency_interp)
+            tape.watch(features_interp)
+            logits = self.discriminator(
+                [adjacency_interp, features_interp], training=True
+            )
+
+        # Compute the gradients with respect to the interpolated graphs
+        grads = tape.gradient(logits, [adjacency_interp, features_interp])
+        # Compute the gradient penalty
+        grads_adjacency_penalty = (1 - tf.norm(grads[0], axis=1)) ** 2
+        grads_features_penalty = (1 - tf.norm(grads[1], axis=2)) ** 2
+        return tf.reduce_mean(
+            tf.reduce_mean(grads_adjacency_penalty, axis=(-2, -1))
+            + tf.reduce_mean(grads_features_penalty, axis=(-1))
+        )
