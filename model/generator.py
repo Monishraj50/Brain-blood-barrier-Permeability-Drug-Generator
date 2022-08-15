ba8c82ccd37fb73811b56354adb9b271a1cac70e
Monish <53029918+Monishraj50@users.noreply.github.com>
2022-08-15 12:51:59 +0530

    Create generator.py

---
+++ model/generator.py
@@ -0,0 +1,33 @@
+def GraphGenerator(
+    dense_units, dropout_rate, latent_dim, adjacency_shape, feature_shape,
+):
+    z = keras.layers.Input(shape=(LATENT_DIM,))
+    # Propagate through one or more densely connected layers
+    x = z
+    for units in dense_units:
+        x = keras.layers.Dense(units, activation="tanh")(x)
+        x = keras.layers.Dropout(dropout_rate)(x)
+
+    # Map outputs of previous layer (x) to [continuous] adjacency tensors (x_adjacency)
+    x_adjacency = keras.layers.Dense(tf.math.reduce_prod(adjacency_shape))(x)
+    x_adjacency = keras.layers.Reshape(adjacency_shape)(x_adjacency)
+    # Symmetrify tensors in the last two dimensions
+    x_adjacency = (x_adjacency + tf.transpose(x_adjacency, (0, 1, 3, 2))) / 2
+    x_adjacency = keras.layers.Softmax(axis=1)(x_adjacency)
+
+    # Map outputs of previous layer (x) to [continuous] feature tensors (x_features)
+    x_features = keras.layers.Dense(tf.math.reduce_prod(feature_shape))(x)
+    x_features = keras.layers.Reshape(feature_shape)(x_features)
+    x_features = keras.layers.Softmax(axis=2)(x_features)
+
+    return keras.Model(inputs=z, outputs=[x_adjacency, x_features], name="Generator")
+
+
+generator = GraphGenerator(
+    dense_units=[512, 1024, 2048],
+    dropout_rate=0.2,
+    latent_dim=LATENT_DIM,
+    adjacency_shape=(BOND_DIM, NUM_ATOMS, NUM_ATOMS),
+    feature_shape=(NUM_ATOMS, ATOM_DIM),
+)
+generator.summary()
